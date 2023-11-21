70df31eed3351691337ccc1733896dbe438a4800
mmiller <martin.miller@stud.htwk-leipzig.de>
2023-11-21 02:04:21 +0100

    added webhook integration, Sneek is now shown with Webhook

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 import discord
 import os
@@ -8,6 +10,9 @@ from bs4 import BeautifulSoup
 load_dotenv()
 
 bot_token = os.getenv('DISCORD_TOKEN')
+webhook_url = os.getenv('DISCORD_WEBHOOK_URL')
+
+proximity_string = ['Hohe', 'Mittlere', 'Niedrige']
 
 if bot_token is None:
     print("Token not found. Please Make sure the environmental variables are set correctly.")
@@ -41,12 +46,54 @@ else:
                 for movie_list in list_objects:
                     if 1 <= counter <= 3:
                         movie = movie_list.find_all('li')
-                        sneek_preview = movie
+                        movie_string = ''.join(map(str, movie))
+                        sneek_preview = sneek_preview + movie_string + '&&\n'
                     counter = counter + 1
         else:
             print(f'Fehler beim Abrufen der Seite. Statuscode: {response.status_code}')
 
-        await ctx.send(sneek_preview)
+        sneek_preview = sneek_preview.replace('</li><li>', '\n')
+        sneek_preview = sneek_preview.replace('<li>', '')
+        sneek_preview = sneek_preview.replace('</li>', '')
+
+        sub_sneek = sneek_preview.split('&&')
+
+        webhook_movies = {
+            'embeds': [
+                {
+                    'title': f'Aktuelle Sneek Peek Vorhersage',
+                    'color': 16711680,
+                    'footer': {
+                        'text': f'Stand'
+                    },
+                    'fields': [
+                        {
+                            "name": "Hohe Wahrscheinlichkeit",
+                            "value": f'{sub_sneek[0]}',
+                            "inline": True
+                        },
+                        {
+                            "name": "Mittlere Wahrscheinlichkeit",
+                            "value": f'{sub_sneek[1]}',
+                            "inline": False
+                        },
+                        {
+                            "name": "Niedrige Wahrscheinlichkeit",
+                            "value": f'{sub_sneek[2]}',
+                            "inline": True
+                        }
+                    ],
+                    'timestamp': f'{datetime.now().date()}'
+                }
+            ]
+        }
+
+        response = requests.post(webhook_url, json=webhook_movies)
+
+        if response.status_code == 204:
+            print('posted new sneek preview')
+        else:
+            print(response.status_code)
 
 
     bot.run(bot_token)
